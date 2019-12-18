b3d1f0a7ee4d7438b0d219f4e0db15e7f296a74b
jadzia <jadzia@prollective.com>
2019-12-18 22:40:27 +0100

    prezentacja

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; // tylko tutaj importujemy ReactDOM
-import App from './components/App';
-
-ReactDOM.render(
-    <App />, // główny komponent aplikacji
-    document.querySelector('#root') // wskazujemy kontener, w jakim chcemy osadzić aplikację
-);
